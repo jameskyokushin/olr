16665cadd4ae2e2236757e3c709ee7d41d80f0c4
james delacruz <james@kyokushin.com.ph>
2011-08-26 16:15:58 +0800

    modify environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ Devise1::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'karate.heroku.com' }
 end
